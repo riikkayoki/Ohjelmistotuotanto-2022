d6a23840a85e15ffdeca2c5251446bb8bd24b642
riikkayoki <74421508+riikkayoki@users.noreply.github.com>
2022-01-20 18:46:30 +0200

    korjaus varasto.py

--- src/varasto.py
+++ src/varasto.py
@@ -1,11 +1,7 @@
 class Varasto:
     def __init__(self, tilavuus, alku_saldo=0):
-
-
-
         self.tilavuus = max(tilavuus, 0)
         if alku_saldo < 0.0:
-     
             # virheellinen, nollataan
             self.saldo = 0.0
         elif alku_saldo <= tilavuus:
